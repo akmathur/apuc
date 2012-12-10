8dce2d535f703121dfa9913748222cb5c7328062
akmathur <akmathur@gmail.com>
2012-12-10 12:09:14 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apuc::Application.config.secret_token = '97aa9db801f2b54f92ee8f26e00311cfc8f7d305cf50826ae07ac528b6aacf68439aa98889104af9d908b88c22b2343907ed467b76d060a80613f38b11cb7668'
